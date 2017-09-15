0a7f82209b36c9ed4fa1ad06b7203c20de80b900
Dylan Zettler <dylanzettler@gmail.com>
2017-09-15 09:32:38 -0600

    Added and subtracted using getters and setters

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,5 +1,35 @@
 package monster.controller;
 
-public class MonsterController {
+import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
+
+public class MonsterController 
+{
+	public void start()
+	{
+		MarshmallowMonster sample = new MarshmallowMonster();
+		System.out.println(sample);
+		MarshmallowMonster realMonster = new MarshmallowMonster("Scary", 4, 3, 4, true);
+		
+		System.out.println(realMonster);
+		System.out.println("John is hungry, so he is going to eat a tentacle");
+		realMonster.setTentacleAmount(3);
+		System.out.println(realmonster);
+		
+		interactWithTheMonster(realMonster);
+	}
+		
+	private void interactWithTheMonster(MarshmallowMonster currentMonster)
+	{
+		System.out.println(currentMonster.getName() + " wants to know what to eat next");
+		System.out.println(currentMonster.getName() + " suggests arms, they have " + currentMonster.getArmCount());
+		System.out.println("How many do you want to eat?");
+		Scanner myScanner = new Scanner(System.in);
+		int consumed = myScanner.nextInt();
+		currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+		System.out.println("Ok, now " + currentMonster.getName() + "has " currentMonster.getarmCount() + " arms left.");
+		
+		
+	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -1,5 +1,73 @@
 package monster.model;
 
-public class MarshmallowMonster {
-
+public class MarshmallowMonster 
+{
+	//Declaration section for the data members
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	public MarshmallowMonster()
+	{
+		//Unless we specify values all data members
+		//are a zero, false, or null
+	}
+	
+	public MarshmallowMonster(String name, int eyeCount, int arms, double tentacles, boolean hasBloop)
+	
+	public String getName()
+	{
+		return name;
+	}
+	public int getarmCount()
+	{
+		return armCount;
+	}
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount
+	}
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armcount;
+	}
+	public void TentacleAmount(double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	
+	public void setBlop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
+	
+		this.name = name;
+		this.eyeCount = eyeCount;
+		this.armCount = arms;
+		this.tentacleAmount = tentacleCount;
+		this.hasBloop = hasBloop;
+	}
+	
+	public String toString()
+	{
+		String description = "Hi, I am a scary monster! My name is " + name + ", and I have " + eyeCount;
+		description += "eyes and I have " + armCount + "arms, and I have " + tentacleAmount + "tentacles";
+		description += "and my bloop existence is " + hasBloop;
+		
+		return description;
+	}
 }
